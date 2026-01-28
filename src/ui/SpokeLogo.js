10ce19ff2bd4db511dfa22aa9ac7f21b4b2ed3e2
Pablo-Ramirez-Oria <pabloramirezoria@gmail.com>
2026-01-28 10:19:38 +0100

    Revert loading logo to educa360 icon

--- src/ui/SpokeLogo.js
+++ src/ui/SpokeLogo.js
@@ -1,6 +1,6 @@
 import React from "react";
-import educamakerLogo from "../assets/educamaker-logo.png";
+import educa360Icon from "../assets/educa360-icon.png";
 
 export default function SpokeLogo() {
-  return <img src={educamakerLogo} alt="Educamaker" style={{ width: "150px", height: "auto" }} />;
+  return <img src={educa360Icon} alt="Educa360" style={{ width: "150px", height: "auto" }} />;
 }
